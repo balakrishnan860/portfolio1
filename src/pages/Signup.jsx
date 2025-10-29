9289e79196d40cb479dda77bc0ac21fce0b4845c
balakrishnan860 <rbk2072002@gmail.com>
2025-10-29 16:34:44 +0530

    Fixed utils/Validators import path

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../authcontext/AuthContext'
-import { signupSchema } from '../utils/validators'
+import { signupSchema } from '../Utils/Validators.js'
 
 export default function Signup(){
   const { signup } = useAuth()
